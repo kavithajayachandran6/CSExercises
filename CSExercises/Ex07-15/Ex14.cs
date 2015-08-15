752e242642cb0bba5d3e75c96185468fdee0b868
kavithajayachandran6 <kavithajayachandran6@gmail.com>
2015-08-15 21:36:11 +0800

    Finished Exe15

--- CSExercises/Ex07-15/Ex14.cs
+++ CSExercises/Ex07-15/Ex14.cs
@@ -1,10 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 namespace CSExercises
 {
     public class Ex14
     {
         public static void Main(string[] args)
         {
-            
+            Console.WriteLine("Enter the length of side A:");
+            Double sideA = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the length of side B:");
+            Double sideB = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the length of side C:");
+            Double sideC = Double.Parse(Console.ReadLine());
+            double s = (sideA + sideB + sideC) / 2;
+            if ((sideA + sideB) > sideC && (sideB + sideC) > sideA && (sideC + sideA) > sideB)
+            {
+                double area = Math.Sqrt(s * (s - sideA) * (s - sideB) * (s - sideC));
+                Console.WriteLine("Area of triangle=" + area);
+            }
+            else
+                Console.WriteLine("Number keyed in cannot form a triangle");
         }
     }
 }
--- CSExercises/Ex07-15/Ex15.cs
+++ CSExercises/Ex07-15/Ex15.cs
@@ -1,3 +1,8 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 namespace CSExercises
 {
     public class Ex15
@@ -5,6 +10,23 @@ namespace CSExercises
         public static void Main(string[] args)
         {
             
+            
+            Console.WriteLine("Enter a three digit no:");
+            int num=Convert.ToInt32(Console.ReadLine());
+            if (num < 1000 && num > 99)
+            {
+
+
+                int a1 = num / 100;
+                int a2 = (num / 10) % 10;
+                int a3 = num % 10;
+                if((Math.Pow(a1, 3) + Math.Pow(a2, 3) + Math.Pow(a3, 3))==num)
+                {
+                    Console.WriteLine("The number is an armstrong no");
+
+                }else
+                    Console.WriteLine("The number is not an armstrong no");
+            }
         }
     }
 }
